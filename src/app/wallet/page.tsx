787efb139dc4bac1dc37f10df08a77e8570f2d93
xisux <87016182+xisux@users.noreply.github.com>
2023-10-17 21:59:23 +0100

    trying to deploy a safe

--- src/app/wallet/page.tsx
+++ src/app/wallet/page.tsx
@@ -6,7 +6,11 @@ import { HeaderSimple } from "../../../components/HeaderSimple/HeaderSimple";
 import { FeaturesCard } from "../../../components/FeaturesCard/FeaturesCard";
 import { FooterSimple } from "../../../components/FooterSimple/FooterSimple";
 
-import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
+import {
+  CHAIN_NAMESPACES,
+  IBaseProvider,
+  SafeEventEmitterProvider,
+} from "@web3auth/base";
 import { Web3Auth } from "@web3auth/modal";
 
 import { useEffect, useState } from "react";
@@ -14,12 +18,20 @@ import { useEffect, useState } from "react";
 import { ADAPTER_EVENTS, UserInfo, WALLET_ADAPTERS } from "@web3auth/base";
 import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
 import { Web3AuthOptions } from "@web3auth/modal";
-
+import { EthersAdapter } from "@safe-global/protocol-kit";
+import { ethers } from "ethers";
 import {
   AuthKitSignInData,
   Web3AuthModalPack,
   Web3AuthEventListener,
 } from "../../../authHelpers";
+
+import SafeApiKit from "@safe-global/api-kit";
+import { SafeFactory } from "@safe-global/protocol-kit";
+import { SafeAccountConfig } from "@safe-global/protocol-kit";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectedHandler: Web3AuthEventListener = (data) =>
   console.log("CONNECTED", data);
@@ -27,7 +39,6 @@ const disconnectedHandler: Web3AuthEventListener = (data) =>
   console.log("DISCONNECTED", data);
 
 export default function UserWallet() {
-
   const [web3AuthModalPack, setWeb3AuthModalPack] =
     useState<Web3AuthModalPack>();
   const [safeAuthSignInResponse, setSafeAuthSignInResponse] =
@@ -125,7 +136,6 @@ export default function UserWallet() {
 
     const userInfo = await web3AuthModalPack.getUserInfo();
     console.log("USER INFO: ", userInfo);
-
     setSafeAuthSignInResponse(signInInfo);
     setUserInfo(userInfo || undefined);
     setProvider(web3AuthModalPack.getProvider() as SafeEventEmitterProvider);
@@ -138,6 +148,49 @@ export default function UserWallet() {
 
     setProvider(null);
     setSafeAuthSignInResponse(null);
+  };
+
+  const createSafe = async () => {
+    try {
+      const provider = new ethers.providers.JsonRpcProvider(
+        "https://rpc.ankr.com/eth_goerli"
+      );
+      const sponsor = new ethers.Wallet(process.env.SPONSOR!, provider);
+      console.log("sponsor", sponsor);
+      const privateKey = await web3AuthModalPack?.getProvider()?.request({
+        method: "eth_private_key",
+      });
+      console.log("privateKey", privateKey);
+      const signer = new ethers.Wallet(privateKey, provider);
+      console.log("signer", signer);
+      const ethAdapterOwner = new EthersAdapter({
+        ethers,
+        signerOrProvider: signer,
+      });
+      console.log("ethAdapterOwner", ethAdapterOwner);
+      const txServiceUrl = "https://safe-transaction-goerli.safe.global";
+      const safeService = new SafeApiKit({
+        txServiceUrl,
+        ethAdapter: ethAdapterOwner,
+      });
+      console.log("safeService", safeService);
+      const safeFactory = await SafeFactory.create({
+        ethAdapter: ethAdapterOwner,
+      });
+      console.log("safeFactory", safeFactory);
+      const safeAccountConfig: SafeAccountConfig = {
+        owners: [await signer.getAddress()],
+        threshold: 1, // We dont need multisig functionality
+      };
+      console.log("safeAccountConfig", safeAccountConfig);
+      const safeSdkOwner = await safeFactory.deploySafe({ safeAccountConfig });
+      const safeAddress = await safeSdkOwner.getAddress();
+      console.log("Your Safe has been deployed:");
+      console.log(`https://goerli.etherscan.io/address/${safeAddress}`);
+      console.log(`https://app.safe.global/gor:${safeAddress}`);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -154,7 +207,7 @@ export default function UserWallet() {
         {safeAuthSignInResponse?.safes?.length === 0 ? (
           <>
             <Text>No Safes - Create one?</Text>
-            <Button>Create Safe</Button>
+            <Button onClick={createSafe}>Create Safe</Button>
           </>
         ) : (
           <Text>Safe</Text>
