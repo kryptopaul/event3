d43e88cd4faf06d0bf66b5804b4c59b5f3a51921
xisux <87016182+xisux@users.noreply.github.com>
2023-10-21 02:26:55 +0100

    mock onramp

---
+++ src/app/api/onramp/route.tsx
@@ -0,0 +1,25 @@
+import { ethers } from "ethers";
+import { NextRequest, NextResponse } from "next/server";
+import dotenv from "dotenv";
+dotenv.config();
+
+const route = async function onramp(req: NextRequest) {
+  const sponsor = process.env.SPONSOR!;
+  const data = await req.json();
+  const { address } = data;
+  console.log("address", address);
+  console.log("sponsor", sponsor);
+  const provider = new ethers.providers.JsonRpcProvider(
+    "https://rpc.ankr.com/eth_goerli"
+  );
+  const signerWallet = new ethers.Wallet(sponsor, provider);
+  const tx = await signerWallet.sendTransaction({
+    to: address,
+    value: ethers.utils.parseEther("0.005"),
+  });
+  await tx.wait(1);
+  console.log("tx", tx);
+  return NextResponse.json("success");
+};
+
+export const POST = route;
